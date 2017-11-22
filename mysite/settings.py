a3f6c69522c0932ef73dd2cd1ab0c307ec0f2656
Svitlana Chornei <svitlanachornei@gmail.com>
2017-11-23 00:36:56 +0200

    s

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,8 +25,8 @@ import os
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
+DEBUG = True
+# DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = ['mysterious-anchorage-39270.herokuapp.com', '127.0.0.1']
 
